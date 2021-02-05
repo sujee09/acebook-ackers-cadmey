102642ffccd7c40593a9ac76dcbd2fec25d77cf6
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-05 12:05:56 +0000

    sesssion controller test coverage

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -13,10 +13,23 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
-  describe "Get /create" do
+  describe "Get /new" do
     it 'responds with 200' do
       get :new
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "Post /create" do
+    subject { get :new }
+    it 'responds with 302' do
+      post :create, params: { session: { email: "will@will.com", password: "hello12"  } }
+      expect(response).to have_http_status(302)
+    end
+
+    it 're renders new if incorrect login' do
+      post :create, params: { session: { email: "will@will.com", password: "hello10"  } }
+      expect(subject).to render_template(:new)
     end
   end
 end

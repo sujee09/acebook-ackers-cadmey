44168d8f465b5cb57f91ce5031fcc32f74354bfe
AJ <aj.monty@gmail.com>
2021-02-05 14:09:36 +0000

    replaced delete button

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,168 +1,123 @@
-function renderPosts() {
-  fetch('/posts_api')
-    .then(response => response.json())
-    .then(data => {
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
 
-      let divPosts = document.getElementById("posts")
-      divPosts.innerHTML = ''
-      data.forEach(element => {
-          let div = document.createElement('div')
-          div.setAttribute('id', 'post')
-          let user = `<p id="user_name">${element.user_name}</p><br>`
-          let message = `<p id="message">${element.message}</p><br>`
-          let time = `<p id="time">${element.short_time}</p><br>`
-          let likes = `<p id="no_of_likes">Likes:${element.number_of_likes}</p><br>`
-          let like_button = document.createElement('div')
-          like_button.setAttribute('id', 'likeButton')
-          div.innerHTML = user + time + message + likes
-          ReactDOM.render(e(LikeButton, {post_id: element.id}),  like_button);
-          div.appendChild(like_button)
-          divPosts.appendChild(div)
+console.log("I'm in users")
+function renderPosts() {
+    fetch('/user_posts_api')
+      .then(response => response.json())
+      .then(data => {
+
+        let divPosts = document.getElementById("posts")
+        divPosts.innerHTML = ''
+        data.forEach(element => {
+            let div = document.createElement('div')
+            div.setAttribute('id', 'post')
+            let user = `<p id="user_name">${element.user_name}</p><br>`
+            let message = `<p id="message">${element.message}</p><br>`
+            let time = `<p id="time">${element.short_time}</p><br>`
+            let likes = `<p id="no_of_likes">Likes:${element.number_of_likes}</p><br>`
+            let delete_button = document.createElement('div')
+            delete_button.setAttribute('id', 'deleteButton')
+            div.innerHTML = user + time + message + likes
+            ReactDOM.render(e(DeleteButton, {post_id: element.id}),  delete_button);
+            div.appendChild(delete_button)
+            divPosts.appendChild(div)
+        });
       });
-    });
-  };
+    };
 
 
 renderPosts()
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true,
-      liked: false,
-    };
-    this.handleClick = this.handleClick.bind('this');
-    this.fetchLikeData()
-  }
-
-  fetchLikeData() {
-    const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-    fetch("/likes/data", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrf
-      },
-      body: JSON.stringify({ post_id: this.props.post_id })
-    })
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      this.setState(data)
-    })
-  }
-  handleClick = () => {
-    this.setState(prevState => (prevState, {
-      liked: !prevState.liked
-    }));
-    this.sendLikeData()
-  }
-
-  render() {
-    if (this.state.loading) {
-      return ''
-    }
-    return e(
-        'i',
-        { onClick: () =>  this.handleClick()  },
-        this.state.liked ? e("span", {className: "fas fa-thumbs-up fa-lg"}) : e("span", {className: "far fa-thumbs-up fa-lg"})
-        );
+class DeleteButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind('this');
     }
 
+    handleClick = () => {
+      this.sendDeleteData()
+    }
 
+    render() {
+      return e(
+          'i',
+          { onClick: () =>  this.handleClick() },
+          e("span", {className: "fas fa-trash-alt"})
+          );
+      }
 
-  sendLikeData() {
-    if (this.state.liked) {
-      const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-      fetch("/likes/destroy", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': csrf
-        },
-        body: JSON.stringify({ post_id: this.props.post_id})
+    sendDeleteData() {
+        const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+        fetch("/posts/destroy", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-Token': csrf
+          },
+          body: JSON.stringify({ post_id: this.props.post_id})
+        })
+        .then(response => {
+        renderPosts()
       })
-      .then(response => {
-      renderPosts()
-    })
-  } else {
-      const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-      fetch("/likes/create", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': csrf
-        },
-        body: JSON.stringify({ post_id: this.props.post_id})
-      })
-      .then(response => {
-      renderPosts()
-    })
-  }
-}
+    }
 
 }
 
 class PostMessage extends React.Component {
-constructor(props) {
-  super(props);
-  this.handleClick = this.handleClick.bind('this');
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind('this');
 
-}
-
-handleClick = () => {
-  const messageValue = document.getElementById('new-message-box').value
-  if (messageValue !== '') {
-    this.sendPost()
-    document.getElementById('new-message-box').value = '';
-    refreshMessageBox();
   }
 
-}
+  handleClick = () => {
+    const messageValue = document.getElementById('new-message-box').value
+    if (messageValue !== '') {
+      this.sendPost()
+      document.getElementById('new-message-box').value = '';
+      refreshMessageBox();
+    }
 
-render() {
-  return [e(
-          'textarea',
-          { rows: '1', id: 'new-message-box', placeholder: "What's on your mind?"},
-          null
-        ),
-        e(
-          'br',
-        ),
-        e(
-            'button',
-            { onClick: () =>  this.handleClick() }, 
-            e("span", {className: "fa fa-paper-plane fa-lg"})
+  }
+
+  render() {
+    return [e(
+            'textarea',
+            { rows: '1', id: 'new-message-box', placeholder: "What's on your mind?"},
+            null
           ),
-        ];
+          e(
+              'button',
+              { onClick: () =>  this.handleClick() },
+              'Create Post'
+            ),];
+    }
+
+
+
+  sendPost() {
+      const messageText = document.getElementById('new-message-box').value
+      const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+      fetch("/posts", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrf
+        },
+        body: JSON.stringify({ message: messageText})
+      })
+      .then(response => {
+      renderPosts();
+      })
+
   }
-
-
-
-sendPost() {
-    const messageText = document.getElementById('new-message-box').value
-    const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-    fetch("/posts", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrf
-      },
-      body: JSON.stringify({ message: messageText})
-    })
-    .then(response => {
-    renderPosts();
-    })
-
-}
 }
 
 function refreshMessageBox() {
-const postContainer = document.getElementById('create-new-post')
-ReactDOM.render(e(PostMessage), postContainer)
+  const postContainer = document.getElementById('create-new-post')
+  ReactDOM.render(e(PostMessage), postContainer)
 }
 refreshMessageBox();
